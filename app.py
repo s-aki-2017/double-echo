0d3f5ca46860038c597e20cf5a64a37cdd4338bb
s-aki-2017 <s.akiyoshi.cel@gmail.com>
2021-09-28 21:26:12 +0900

    in progress

--- app.py
+++ app.py
@@ -18,20 +18,14 @@ app = Flask(__name__)
 
 
 #環境変数からLINE Access Tokenを設定
-
 LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 #環境変数からLINE Channel Secretを設定
-
 LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 
-#APP_URL = 'https://double-echo.herokuapp.com'
-APP_URL = ''
-#APP_URL = '/Users/s_aki/develop/double-echo'
+APP_URL = 'https://double-echo.herokuapp.com'
 SRC_IMAGE_PATH = "static/images/{}.jpg"
 MAIN_IMAGE_PATH = "static/images/{}_main.jpg"
 PREVIEW_IMAGE_PATH = "static/images/{}_preview.jpg"
-
-
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
@@ -78,10 +72,6 @@ def handle_image(event):
         original_content_url=f'{APP_URL}/{src_image_path}',
         preview_image_url=f'{APP_URL}/{src_image_path}'
     )
-    # image_message = ImageSendMessage(
-    #     original_content_url=f'{PROC_URL}',
-    #     preview_image_url=f'{PROC_URL}'
-    # )
 
     # error
     # app.logger.info(f"{APP_URL}/{main_image_path}")
