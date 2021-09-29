518b6533fc4be07de173a79485ea165596d35a02
s-aki-2017 <62897463+s-aki-2017@users.noreply.github.com>
2021-09-29 23:58:58 +0900

    Revert "画像を送るとグレースケールで返してくれるように実装"

--- ImageProcessor.py
+++
@@ -1,5 +0,0 @@
-import cv2
-
-def to_gray(image_path):
-    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    cv2.imwrite(image_path, img)
--- app.py
+++ app.py
@@ -12,23 +12,20 @@ from linebot.models import (
 )
 import os
 from pathlib import Path
-import time
-import ImageProcessor
 
 # 軽量なウェブアプリケーションフレームワーク:Flask
 app = Flask(__name__)
 
 
 #環境変数からLINE Access Tokenを設定
-os.environ["LINE_CHANNEL_ACCESS_TOKEN"] = "aGU/TQDmcKFz5zk6G2qq4aZD9yr/c2oTtrxmPh/ZufxhGym6nTDOjrwU9krvPp96GRFnWzSmhri75JlUW8TIp8V/yZAAX+doTz7by3akxZXlTa6UPzhCZt9s0vvHRSzHMHKX/gN1PJDF+eIEV5xTIwdB04t89/1O/w1cDnyilFU="
 LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 #環境変数からLINE Channel Secretを設定
-os.environ["LINE_CHANNEL_SECRET"] = "f3827c2bbdfdd79eaa10f8892dc92ce6"
 LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 
-#APP_URL = 'https://double-echo.herokuapp.com'
-APP_URL = 'https://9446-116-70-174-11.ngrok.io'
+APP_URL = 'https://double-echo.herokuapp.com'
 SRC_IMAGE_PATH = "static/images/{}.jpg"
+MAIN_IMAGE_PATH = "static/images/{}_main.jpg"
+PREVIEW_IMAGE_PATH = "static/images/{}_preview.jpg"
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
@@ -60,25 +57,38 @@ def handle_message(event):
 
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image(event):
-    src_image_path = Path(SRC_IMAGE_PATH.format(event.message.id)).absolute()
-    message_content = line_bot_api.get_message_content(event.message.id)
-    with open(src_image_path, "wb") as f:
-        f.write(message_content.content)
+    message_id = event.message.id
+
+    src_image_path = Path(SRC_IMAGE_PATH.format(message_id)).absolute()
+    main_image_path = MAIN_IMAGE_PATH.format(message_id)
+    preview_image_path = PREVIEW_IMAGE_PATH.format(message_id)
+
+    #message_content = line_bot_api.get_message_content(message_id)
+
+    # 画像を保存
+    save_image(message_id, src_image_path)
     
-    ImageProcessor.to_gray(str(src_image_path))
     image_message = ImageSendMessage(
-        original_content_url=APP_URL+str(src_image_path),
-        #preview_image_url=APP_URL+"/static/images/"+event.message.id+".jpg"
-        preview_image_url=APP_URL+str(src_image_path)
+        original_content_url=f'{APP_URL}/{src_image_path}',
+        preview_image_url=f'{APP_URL}/{src_image_path}'
     )
+
+    # error
+    app.logger.info(f"{APP_URL}/{main_image_path}")
     line_bot_api.reply_message(
         event.reply_token,
         image_message
     )
-    time.sleep(1.5)
-    # 画像を削除する
-    #src_image_path.unlink()
 
+    # 画像を削除する
+    src_image_path.unlink()
+
+def save_image(message_id: str, save_path: str) -> None:
+    """保存"""
+    message_content = line_bot_api.get_message_content(message_id)
+    with open(save_path, "wb") as f:
+        for chunk in message_content.iter_content():
+            f.write(chunk)
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
